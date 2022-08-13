2bfb20bd7f68b66f4b52e881762a0beefb1a4b70
kajoul <kaisajoulu@gmail.com>
2022-08-13 23:48:18 +0300

    navbar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyArh8IcBx3EIjkHtp-GbhHbkhu2566SlsQ",
+    authDomain: "netflix-clone-37d79.firebaseapp.com",
+    projectId: "netflix-clone-37d79",
+    storageBucket: "netflix-clone-37d79.appspot.com",
+    messagingSenderId: "780431538657",
+    appId: "1:780431538657:web:2a8a767675e92ffa8f3aa0"
+  };
